46a1cd2fda55b56b3a6e822afcdbebb366176d76
Jeroen Ooms <jeroenooms@gmail.com>
2016-02-28 20:47:49 +0100

    Workarounds for Solaris

--- src/config.h
+++ src/config.h
@@ -2,3 +2,10 @@
 #ifdef WORDS_BIGENDIAN
 #define BROTLI_BUILD_BIG_ENDIAN
 #endif
+
+/* some hack for solaris 10 */
+#if defined __SUNPRO_CC && !defined __RESTRICT
+#include <stdlib.h>
+# define _Restrict
+# define __restrict
+#endif
